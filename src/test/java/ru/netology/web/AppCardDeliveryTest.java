22dc8f4940c694b5b5dfaac1bf002138cf35da66
Shliskenstickin <deathwalker@mail.ru>
2021-05-27 14:00:30 +0300

    add selenide

---
+++ src/test/java/ru/netology/web/AppCardDeliveryTest.java
@@ -0,0 +1,28 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AppCardDeliveryTest {
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void test01() {
+        $("[data-test-id=\"city\"] [class=\"input__control\"]").setValue("Москва");
+//        $("[data-test-id=\"date\"]").setValue("Москва");
+        $("[data-test-id=\"name\"] [class=\"input__control\"]").setValue("Иван");
+        $("[data-test-id=\"phone\"] [class=\"input__control\"]").setValue("+71112223344");
+        $("[data-test-id=\"agreement\"] [class=\"checkbox__box\"]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
