57f7bcffc2a5d1cdac6aa16c341a93b48f92ba93
Shliskenstickin <deathwalker@mail.ru>
2021-05-27 16:30:05 +0300

    add tests

--- src/test/java/ru/netology/web/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/web/AppCardDeliveryTest.java
@@ -2,6 +2,7 @@ package ru.netology.web;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class AppCardDeliveryTest {
     @BeforeEach
@@ -17,14 +19,106 @@ public class AppCardDeliveryTest {
     }
 
     @Test
-    void test01() {
+    void shouldValidData() {
         $("[data-test-id=\"city\"] [class=\"input__control\"]").setValue("Москва");
-//        String date = LocalDate.now().plusDays(4).toString();
-//        $("[data-test-id=\"date\"]").setValue(date);
+        for (int i = 0; i < 9; i++){
+            $("[data-test-id=\"date\"] [class=\"input__control\"]").sendKeys(Keys.BACK_SPACE);
+        }
+        String date = LocalDate.now().plusDays(4).format(ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=\"date\"] [class=\"input__control\"]").setValue(date);
         $("[data-test-id=\"name\"] [class=\"input__control\"]").setValue("Иван");
         $("[data-test-id=\"phone\"] [class=\"input__control\"]").setValue("+71112223344");
         $("[data-test-id=\"agreement\"] [class=\"checkbox__box\"]").click();
         $$("button").find(exactText("Забронировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldWarningIfInvalidCity() {
+        $("[data-test-id=\"city\"] [class=\"input__control\"]").setValue("Подмосковье");
+        for (int i = 0; i < 9; i++){
+            $("[data-test-id=\"date\"] [class=\"input__control\"]").sendKeys(Keys.BACK_SPACE);
+        }
+        String date = LocalDate.now().plusDays(4).format(ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=\"date\"] [class=\"input__control\"]").setValue(date);
+        $("[data-test-id=\"name\"] [class=\"input__control\"]").setValue("Иван");
+        $("[data-test-id=\"phone\"] [class=\"input__control\"]").setValue("+71112223344");
+        $("[data-test-id=\"agreement\"] [class=\"checkbox__box\"]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $((".input_invalid")).shouldBe(exist);
+    }
+
+    @Test
+    void shouldWarningIfInvalidName() {
+        $("[data-test-id=\"city\"] [class=\"input__control\"]").setValue("Москва");
+        for (int i = 0; i < 9; i++){
+            $("[data-test-id=\"date\"] [class=\"input__control\"]").sendKeys(Keys.BACK_SPACE);
+        }
+        String date = LocalDate.now().plusDays(4).format(ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=\"date\"] [class=\"input__control\"]").setValue(date);
+        $("[data-test-id=\"name\"] [class=\"input__control\"]").setValue("Ivan");
+        $("[data-test-id=\"phone\"] [class=\"input__control\"]").setValue("+71112223344");
+        $("[data-test-id=\"agreement\"] [class=\"checkbox__box\"]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $((".input_invalid")).shouldBe(exist);
+    }
+
+    @Test
+    void shouldWarningIfInvalidPhone() {
+        $("[data-test-id=\"city\"] [class=\"input__control\"]").setValue("Москва");
+        for (int i = 0; i < 9; i++){
+            $("[data-test-id=\"date\"] [class=\"input__control\"]").sendKeys(Keys.BACK_SPACE);
+        }
+        String date = LocalDate.now().plusDays(4).format(ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=\"date\"] [class=\"input__control\"]").setValue(date);
+        $("[data-test-id=\"name\"] [class=\"input__control\"]").setValue("Иван");
+        $("[data-test-id=\"phone\"] [class=\"input__control\"]").setValue("81112223344");
+        $("[data-test-id=\"agreement\"] [class=\"checkbox__box\"]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $((".input_invalid")).shouldBe(exist);
+    }
+
+    @Test
+    void shouldWarningIfCheckboxOff() {
+        $("[data-test-id=\"city\"] [class=\"input__control\"]").setValue("Москва");
+        for (int i = 0; i < 9; i++){
+            $("[data-test-id=\"date\"] [class=\"input__control\"]").sendKeys(Keys.BACK_SPACE);
+        }
+        String date = LocalDate.now().plusDays(4).format(ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=\"date\"] [class=\"input__control\"]").setValue(date);
+        $("[data-test-id=\"name\"] [class=\"input__control\"]").setValue("Иван");
+        $("[data-test-id=\"phone\"] [class=\"input__control\"]").setValue("+71112223344");
+        $$("button").find(exactText("Забронировать")).click();
+        $((".input_invalid")).shouldBe(exist);
+    }
+
+    @Test
+    void shouldWarningIfPastDate() {
+        $("[data-test-id=\"city\"] [class=\"input__control\"]").setValue("Москва");
+        for (int i = 0; i < 9; i++){
+            $("[data-test-id=\"date\"] [class=\"input__control\"]").sendKeys(Keys.BACK_SPACE);
+        }
+        String date = LocalDate.now().minusDays(1).format(ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=\"date\"] [class=\"input__control\"]").setValue(date);
+        $("[data-test-id=\"name\"] [class=\"input__control\"]").setValue("Иван");
+        $("[data-test-id=\"phone\"] [class=\"input__control\"]").setValue("+71112223344");
+        $("[data-test-id=\"agreement\"] [class=\"checkbox__box\"]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $((".input_invalid")).shouldBe(exist);
+    }
+
+    @Test
+    void shouldWarningIfDayToDay() {
+        $("[data-test-id=\"city\"] [class=\"input__control\"]").setValue("Москва");
+        for (int i = 0; i < 9; i++){
+            $("[data-test-id=\"date\"] [class=\"input__control\"]").sendKeys(Keys.BACK_SPACE);
+        }
+        String date = LocalDate.now().format(ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=\"date\"] [class=\"input__control\"]").setValue(date);
+        $("[data-test-id=\"name\"] [class=\"input__control\"]").setValue("Иван");
+        $("[data-test-id=\"phone\"] [class=\"input__control\"]").setValue("+71112223344");
+        $("[data-test-id=\"agreement\"] [class=\"checkbox__box\"]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $((".input_invalid")).shouldBe(exist);
     }
 }
